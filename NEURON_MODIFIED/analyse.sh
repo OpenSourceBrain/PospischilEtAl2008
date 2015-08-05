f43b0f4b0442425b63e470c114925fd5b9c4904c
Padraig Gleeson <p.gleeson@gmail.com>
2015-08-05 12:18:31 +0100

    More analysis scripts

---
+++ NEURON_MODIFIED/analyse.sh
@@ -0,0 +1,6 @@
+
+pynml-modchananalysis im -stepV 10 -temperature 36 -modFile IM_cortex.mod
+
+
+
+
---
+++ NeuroML2/channels/analyse_chans.sh
@@ -0,0 +1,5 @@
+pynml-channelanalysis Na.channel.nml \
+                      IT.channel.nml \
+                      IM.channel.nml \
+                      IL.channel.nml \
+                      -caConc 4.3e-4 -temperature 23 -html
